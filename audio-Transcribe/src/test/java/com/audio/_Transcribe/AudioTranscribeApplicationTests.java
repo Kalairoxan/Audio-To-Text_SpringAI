2f6034baa4ad6e2be6bf2cde2cabc1ddbef55566
Kalaiyarasan D <135841909+Kalairoxan@users.noreply.github.com>
2024-12-14 15:06:32 +0530

    Add files via upload

---
+++ audio-Transcribe/src/test/java/com/audio/_Transcribe/AudioTranscribeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio._Transcribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
